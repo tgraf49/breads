5120016cf964da3d115eccde386c54fd15c8082d
Tyler Graf <tygraf13@gmail.com>
2023-10-24 19:27:13 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT;
 console.log(PORT)
 
+// Middleware
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
---
+++ views/index.jsx
@@ -0,0 +1,24 @@
+const React = require('react')
+const Def = require('./layouts/default')
+
+function Index ({breads}) {
+    return (
+        <Def>
+            <h2>Index Page</h2>
+            {/* <p>I have {breads[0].name} bread!</p> */}
+            <ul>
+                {
+                    breads.map((bread, index) => {
+                        return (<li key={index}>
+                            <a href={`/breads/${index}`}>
+                            {bread.name}
+                            </a>
+                        </li>)
+                    })
+                }
+            </ul>
+        </Def>
+    )
+}
+
+module.exports = Index
